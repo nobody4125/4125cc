6e07dfcaa099d7633936967842aefd77c7405d89
nobody4125 <nobody4125@gmail.com>
2019-07-05 20:00:45 +0000

    単項+、-を追加

--- 4125cc.c
+++ 4125cc.c
@@ -118,6 +118,7 @@ int consume(int ty) {
 Node *expr();
 Node *mul();
 Node *term();
+Node *unary();
 
 Node *expr() {
     Node *node = mul();
@@ -135,17 +136,27 @@ Node *expr() {
 
 
 Node *mul() {
-    Node *node = term();
+    Node *node = unary();
 
     while (1) {
         if (consume('*')) {
-            node = new_node('*', node, term());
+            node = new_node('*', node, unary());
         } else if (consume('/')) {
-            node = new_node('/', node, term());
+            node = new_node('/', node, unary());
         } else {
             return node;
         }
     }
+}
+
+Node *unary() {
+    if (consume('+')) {
+        return term();
+    }
+    if (consume('-')) {
+        return new_node('-', new_node_num(0), term());
+    }
+    return term();
 }
 
 Node *term() {
